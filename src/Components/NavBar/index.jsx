bddd5d53492caa3047ec80c97e7218a65527e51d
MageLink <mage.link.zhezard@gmail.com>
2022-09-24 11:38:29 -0300

    Header logo perfect

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,5 +1,19 @@
+import { IconContext } from "react-icons";
+import { RiCake2Fill } from "react-icons/ri";
 const NavBar = () => {
-  return <div className="navbar-container">NavBar o k ase</div>;
+  return (
+    <div className="navbar-container">
+      <div className="navbar-options-container">
+        <span>Uno</span>
+        <span>Dos</span>
+      </div>
+      <IconContext.Provider value={{ className: "NavBar-cake-icon" }}>
+        <div className="navbar-logo-container">
+          Happy Cake <RiCake2Fill />
+        </div>
+      </IconContext.Provider>
+    </div>
+  );
 };
 
 export default NavBar;
--- src/Context/GeneralContext/Provider.js
+++ src/Context/GeneralContext/Provider.js
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 // Context
 import GeneralContext from ".";
 // Utilities
-import apiCall from "../../api/apiCall";
+//import apiCall from "../../api/apiCall";
 
 const GeneralContextProvider = ({ children }) => {
   const [test, setTest] = useState(true);
 
-  const exampleUseApiCall = async (userId) => {
-    try {
-      const data = await apiCall({ url: `http://insertApiAddress` });
-      // Insert setData(data)
-    } catch (e) {
-      alert("Un error ha ocurrido. Por favor actualice la página");
-    }
-  };
+  //const exampleUseApiCall = async (userId) => {
+  //  try {
+  //    const data = await apiCall({ url: `http://insertApiAddress` });
+  //    // Insert setData(data)
+  //  } catch (e) {
+  //    alert("Un error ha ocurrido. Por favor actualice la página");
+  //  }
+  //};
 
   return <GeneralContext.Provider value={{ test, setTest }}>{children}</GeneralContext.Provider>;
 };
