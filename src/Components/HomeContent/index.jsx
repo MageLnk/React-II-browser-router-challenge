2254153ce64ba010ba09214c9be49c95ed11d06b
MageLink <mage.link.zhezard@gmail.com>
2022-09-24 12:12:08 -0300

    Home done. Se podría ponen más cosas obviamente

--- src/Components/HomeContent/index.jsx
+++ src/Components/HomeContent/index.jsx
@@ -1,5 +1,12 @@
 const HomeContent = () => {
-  return <div>Home o k ase</div>;
+  return (
+    <div className="home-content-container">
+      <h1>
+        Bienvenido a <span>Happy Cake</span>
+      </h1>
+      <span>El lugar donde se hacen los pateles felices</span>
+    </div>
+  );
 };
 
 export default HomeContent;
