cacf0ce19b5d3f553266e77a30f00a4f60ee2d7c
MageLink <mage.link.zhezard@gmail.com>
2022-09-24 11:58:24 -0300

    Error 404 improve

--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -1,5 +1,9 @@
 const Error404 = () => {
-  return <div>Error404 o k ase</div>;
+  return (
+    <div className="error-404-container">
+      <h1>Error 404</h1>
+    </div>
+  );
 };
 
 export default Error404;
